be655ad24ff7c699f1f242eb429b4ef39604d588
Oleg152 <aforusinski@gmai.com>
2021-12-10 13:25:58 +0100

    1.3.2 Fixed formatting errors in files

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,16 +2,19 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+	int totalRejected = 0;
 
 	public int reportRejected() {
 		return totalRejected;
 	}
 
 	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
+		if (!callCheck() && in > peekaboo())
 			totalRejected++;
-			else
-				super.countIn(in);
+		else
+			super.countIn(in);
 	}
 }
+//  File edited in Eclipse, shortcuts do following things:
+//	alt + left arrow key switches to previously opened file
+//	alt + right arrow key switches in opposite direction
