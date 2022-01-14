68ab88f9b12e2e96a41610eabb65cadb62909c97
Oleg152 <aforusinski@gmai.com>
2022-01-14 08:37:57 +0100

    1.4.3 changed public variables to private variables in DefaultCountingOutRhymer.java,FIFORhymer.java,HanoiRhymer.java; changed public methods to private methods where it didn't cause errors

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery;
 
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
-	public DefaultCountingOutRhymer countingRhymer = new DefaultCountingOutRhymer();
+	private DefaultCountingOutRhymer countingRhymer = new DefaultCountingOutRhymer();
 
 	@Override
 	public int countOut() {
