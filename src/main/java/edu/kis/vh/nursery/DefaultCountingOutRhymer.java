4ff8c18925fa726df76459b1e61b880ca2b004fb
Veanden <marcin.bros17@gmail.com>
2022-01-27 17:49:29 +0100

    1.4 stale 2 Changed variables to final in DefaultCountingOutRhymer.java, FIFORhymer.java and Node.java

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -6,7 +6,7 @@ public class DefaultCountingOutRhymer {
 	public static final int ERROR = -1;
 	public static final int FULL = 11;
 
-	private int[] numbers = new int[SIZE];
+	private final int[] numbers = new int[SIZE];
 
 	private int total = ERROR;
 
